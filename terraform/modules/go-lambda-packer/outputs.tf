d321310c71b038b97537c60a54eeb2fb6c4cc8d0
tiborhercz <tibor@hercz.nl>
2024-08-06 15:08:06 +0200

    Upgraded TF versions and moved terraform into terraform folder

--- terraform/modules/go-lambda-packer/outputs.tf
+++ terraform/modules/go-lambda-packer/outputs.tf
@@ -5,5 +5,5 @@ output "source_code_hash" {
 
 output "archive_output_path" {
   description = "Path of the zip file that contains the Go binary."
-  value       = data.archive_file.zip.output_path
+  value       = data.external.go_lambda_packer.result.output_path
 }
